dfc92149304bb734a541736f62b9ae10f987b32a
Shape Ous <Shapeous@yahoo.com>
2015-07-16 00:24:59 -0400

    Installed gulp-compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require("gulp")
   , util = require("gulp-util")
   , conn = require("gulp-connect")
+  , sass = require("gulp-compass")
+  , del  = require("gulp-clean-dest")
   ;
 
 // sources
 var appRoot = "dev/"
+  , srcRoot = "src/"
   , htmlSrc = appRoot + "*.html"
+  , sassSrc = srcRoot + "sass/"
   ;
 
 gulp.task("connect", function() {
@@ -22,11 +26,27 @@ gulp.task("html", function() {
     .pipe(conn.reload());
 });
 
+gulp.task("compass", function() {
+  // I transpile SASS files into CSS
+  gulp.src(sassSrc + "style.scss")
+    .pipe(sass(
+      { "sass":	sassSrc
+      , "style": "expanded"
+      , "comments": true
+      })
+      .on("error", util.log)
+    )
+    .pipe(gulp.dest(appRoot + "css"))
+    .pipe(conn.reload())
+    .pipe(del("css"))	// Deleting "css/style.css" allows style to be changed from one run to the next
+});
+
 gulp.task("watch", function() {
   // I watch for file changes and run tasks
   gulp.watch(htmlSrc, ["html"]);
+  gulp.watch(sassSrc + "*.scss", ["compass"]);
 });
 
-gulp.task("default", ["connect", "html", "watch"], function() {
+gulp.task("default", ["connect", "html", "compass", "watch"], function() {
   util.log("- I loaded all above tasks ;)");
 });
